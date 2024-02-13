67e9190f65712aeed8845b665246b498a262c8f5
Ayush kumar jha <ayushkmrjha@yahoo.com>
2024-02-13 16:01:51 +0530

    Add files via upload

---
+++ richierich/richierich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  richierich
+//
+//  Created by Ayush kumar Jha on 13/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
